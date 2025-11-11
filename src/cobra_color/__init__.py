31f2a3dfbf7df1440c6592e74335c7cf9abce1e1
tinchen777 <59043279+tinchen777@users.noreply.github.com>
2025-11-11 19:21:18 +0800

    fix version

--- src/cobra_color/__init__.py
+++ src/cobra_color/__init__.py
@@ -6,7 +6,34 @@ cobra-color
 A lightweight Python package for terminal display enhancements.
 
 _Example:_
->>> from cobra_color import ctext
+- Render a text in the terminal:
+
+```python
+from cobra_color import ctext
+
+print(ctext("Hello World!", fg="r", styles=["bold"]))
+```
+
+- Render an image in the terminal:
+
+```python
+from cobra_color.draw import fmt_image
+
+# ASCII art
+print(fmt_image("example.jpg", width=80, mode="ascii"))
+
+# Half-block color (recommended for truecolor terminals)
+print(fmt_image("example.jpg", width=80, mode="half-color"))
+```
+
+- Render some text with fonts in the terminal:
+
+```python
+from cobra_color.draw import fmt_font, FontName
+
+# Borderless grayscale font
+print(fmt_font("Hello World!", font=FontName.LLDISCO,, mode="half-gray", trim_border=True))
+```
 """
 from . import draw
 
@@ -17,7 +44,7 @@ from .format import fmt_dict, fmt_list
 
 
 __author__ = "Zhen Tian"
-__version__ = "0.1.0"
+__version__ = "0.2.2"
 
 __all__ = [
     "draw",  # module
